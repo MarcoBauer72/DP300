4e470e7077b4e0d7564f857c4075422671aac69c
Marco Bauer <32466827+MarcoBauer72@users.noreply.github.com>
2026-08-06 13:03:54 -0300

    Update X-EXTRA_DataTypes_Issues.sql

--- X-EXTRA_DataTypes_Issues.sql
+++ X-EXTRA_DataTypes_Issues.sql
@@ -1,20 +1,23 @@
 
-
+--------------------------------------------------------------------------------
 --- Exemplo de aplicar CHAR(100) em uma coluna depois move-la para VARCHAR(100)
+--------------------------------------------------------------------------------
 CREATE DATABASE [types]
 GO
 
 USE [types]
 GO
 
-CREATE TABLE dbo.charzao
-(
-id int identity primary key
-,nome char(100)
-)
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'charzao')
+BEGIN
+    CREATE TABLE [dbo].[charzao](
+        [ID] INT IDENTITY(1,1) PRIMARY KEY,
+        [Nome] CHAR(100)
+    );
+END;
 
 declare @i int = 1
-while @i < 2000000
+while @i < 500000
 	begin
 		insert dbo.charzao values ('teste')
 		set @i =+ 1
@@ -23,10 +26,16 @@ while @i < 2000000
 sp_spaceused 'dbo.charzao'
 
 
+ALTER TABLE [dbo].[charzaoa]
+ALTER COLUMN [Nome] VARCHAR(100);
+
+sp_spaceused 'dbo.charzao'
 
 
+--------------------------------------------------------------------------------
 --- Exemplo de comparar coluna com VARCHAR(50)> VARCHAR(1000) e VARCHAR(2000)
 --- e planos de execucao
+--------------------------------------------------------------------------------
 CREATE TABLE dbo.varchar50
 (
 id int identity primary key
@@ -45,8 +54,10 @@ id int identity primary key
 ,nome varchar(2000)
 )
 
+-------------------------------------------------------------------------------------
 --- Em um SQL PaaS criar uma tabela com muitos VARCHAR e outra tabela apartada porem
 --- mesmo schema porem tudo NVARCHAR e comparar tamanho e desempenho
+-------------------------------------------------------------------------------------
 CREATE TABLE dbo.varchartodos
 
 
